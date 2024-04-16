5b8f8443c65ea7f36d2871b8de518bc2860efc17
raky291 <raky291@gmail.com>
2024-04-16 13:33:37 -0700

    update dependencies

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <>
       <h1 className="text-center">Pokédex</h1>
-      {/* <div className="mx-auto" style={{ maxWidth: "700px" }}>
-        <PokemonCard name={"charizard"} />
-      </div> */}
       <PokemonList />
     </>
   );
