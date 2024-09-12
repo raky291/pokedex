9ba3ef227afaaa9f5f9cb0a28c51a99d72625897
raky291 <raky291@gmail.com>
2024-09-11 20:40:34 -0700

    refactor: update types and locales

--- src/app/pokedex/services/types.ts
+++ src/app/pokedex/services/types.ts
@@ -23,14 +23,6 @@ export interface PokemonType {
 }
 
 export interface PokemonSprites {
-  front_default: string | null;
-  front_shiny: string | null;
-  front_female: string | null;
-  front_shiny_female: string | null;
-  back_default: string | null;
-  back_shiny: string | null;
-  back_female: string | null;
-  back_shiny_female: string | null;
   other: OtherPokemonSprites;
 }
 
